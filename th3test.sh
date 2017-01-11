c5aaafe7b2addf8d7e47e5789ca7d45414d79ff9
Th3Fanbus <Th3Fanbus@users.noreply.github.com>
2017-01-11 01:53:58 +0100

    Pushing from local.

--- th3test.sh
+++ th3test.sh
@@ -3,6 +3,7 @@ push() {
     cd bin
     jar cvf th3test.jar .
     cd ..
+    mv bin/th3test.jar th3test.jar
     echo "Uploading to GitHub..."
     git add . && git commit -m 'Pushing from local.' && git push origin master
     echo "Done."
